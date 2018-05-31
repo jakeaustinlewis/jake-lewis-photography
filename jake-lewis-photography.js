8aff0465c9280426929af2063a69d92f9777898e
Jake Lewis <jakeaustinlewis@gmail.com>
2018-05-31 17:00:38 -0500

    added a dynamic navbar

--- jake-lewis-photography.js
+++ jake-lewis-photography.js
@@ -4,6 +4,14 @@ document.addEventListener('DOMContentLoaded', function () {
     let header = document.getElementById('containerHead');
     let photosHead = document.getElementById('photosHead');
     let cameraPicture = document.getElementById('cameraPicture');
+    let topOfPage = document.getElementById('topOfPage');
+    let nav = document.getElementById('content');
+    let positionFixer = document.getElementById('positionFixer');
+
+
+    // positionFixer.style.height = 64;
+    topOfPage.style.height = nav.style.height;
+    // let photosHead
 
     params = {
         "method": "method=flickr.people.getPublicPhotos",
@@ -13,18 +21,23 @@ document.addEventListener('DOMContentLoaded', function () {
         "privacy_filter": "&privacy_filter=5",
         "extras": "&extras=description,date_upload,date_taken,last_update,url_t,url_m,url_l,url_o",
         "format": "&format=json",
-        "nojsoncallback": "&nojsoncallback=1"
+        "nojsoncallback": "&nojsoncallback=1",
+        "secret": "c528a760ce45c265"
     };
 
+    // photosHead.style.height = cameraPicture.style.height + nav.style.height/2;
+
     // Sets the height of the camera picture to the height of the browser window
-    let windowHeight = $(window).innerHeight();
-    $('#cameraPicture').css('min-height', windowHeight);
-    console.log(windowHeight);
+    // let windowHeight = $(window).innerHeight();
+    // $('#cameraPicture').css('min-height', windowHeight);
+    // console.log(windowHeight);
 
 
-    photosHead.addEventListener('click', function (e) {
+    // photosHead.addEventListener('click', function (e) {
+        // e.preventDefault();
+        // });
 
-        e.preventDefault();
+        
         // let link = `https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=b69f4589bad77785ad00bd9e625976bf&user_id=157366842@N07&extras=url_m&per_page=10&format=json&nojsoncallback=1`;
         let link = `https://api.flickr.com/services/rest/?${params.method}${params.api_key}${params.user_id}${params.extras}${params.per_page}${params.format}${params.nojsoncallback}`;
 
@@ -41,22 +54,29 @@ document.addEventListener('DOMContentLoaded', function () {
 
                     // Adding image
                     let photo_element = document.createElement("img");
-                    let photo = data.photos.photo[i].url_m;
+                    let photo = data.photos.photo[i].url_l;
                     photo_element.setAttribute('src', photo);
                     photo_element.classList.add('materialboxed');
 
-                    //margin: auto auto centers photos within dev container
-                    photo_element.style.cssText = `align-self: center; cursor: pointer; max-width: 100%; margin: auto auto`;
+                    // max-height: 470px margin: auto auto centers photos within dev container
+                    photo_element.style.cssText = `align-self: center; cursor: pointer; max-width: 100%; max-height: 263.906px`;
                     myPhotos.appendChild(photo_element);
                 }
                 photos_displayed();
             })
-    });
+    
 
     function photos_displayed() {
         let elems = document.querySelectorAll('.materialboxed');
         let instances = M.Materialbox.init(elems);
+        $(document).ready(function(){
+            $('#content').pushpin({
+              top: $('#content').offset().top 
+            });
+            $('.scrollspy').scrollSpy({
+              scrollOffset: 0
+            });
+          });
     }
 });
-// key b69f4589bad77785ad00bd9e625976bf
-// Secret: c528a760ce45c265
+//calculate height of camera picture
