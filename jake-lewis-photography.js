09f4cf182dba2eff0b03796d1d8b80f8a30d77cc
edpantzar@gmail.com <edpantzar@gmail.com>
2018-09-04 10:42:03 -0500

    Refactored original code

--- jake-lewis-photography.js
+++ jake-lewis-photography.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    params = {
+    const params = {
         "method_publicPhotos": "method=flickr.people.getPublicPhotos",
         "method_albumPhotos": "method=flickr.photosets.getPhotos",
         "api_key": "&api_key=b69f4589bad77785ad00bd9e625976bf",
@@ -13,44 +13,38 @@ document.addEventListener('DOMContentLoaded', function () {
         "photoset_id": "&photoset_id=72157667590992297"
     };
 
-    // let link = `https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=b69f4589bad77785ad00bd9e625976bf&user_id=157366842@N07&extras=url_m&per_page=10&format=json&nojsoncallback=1`;
-    let link_publicPhotos = `https://api.flickr.com/services/rest/?${params.method_publicPhotos}${params.api_key}${params.user_id}${params.extras}${params.per_page}${params.format}${params.nojsoncallback}`;
-    let link_albumPhotos = `https://api.flickr.com/services/rest/?${params.method_albumPhotos}${params.api_key}${params.photoset_id}${params.user_id}${params.extras}${params.per_page}${params.format}${params.nojsoncallback}`;
-
+    const link_publicPhotos = `https://api.flickr.com/services/rest/?${params.method_publicPhotos}${params.api_key}${params.user_id}${params.extras}${params.per_page}${params.format}${params.nojsoncallback}`;
+    const link_albumPhotos = `https://api.flickr.com/services/rest/?${params.method_albumPhotos}${params.api_key}${params.photoset_id}${params.user_id}${params.extras}${params.per_page}${params.format}${params.nojsoncallback}`;
 
     // fetching all pictures in my flickr account
     fetch(link_publicPhotos)
         .then(response => response.json())
         .then(data => {
-            console.log(data);
-            let myPhotos = document.getElementById('myPhotos'); //my Photos is a section containing the populated photos
-            let publicPhotosArray = data.photos.photo;
+
+            //my Photos is a section containing the populated photos
+            const myPhotos = document.getElementById('myPhotos');
+            const publicPhotosArray = data.photos.photo;
 
             //Add photos from API to myPhotos (parent element)
             for (let i = 0; i < publicPhotosArray.length; i++) {
 
                 // Adding image
-                let photo_element = document.createElement("img"); //
-                let photo = publicPhotosArray[i].url_l; //change url_l to url_o for origonal size or url_m for a medium size
-                photo_element.src = photo;
+                let photo_element = document.createElement("img");
+                photo_element.src = publicPhotosArray[i].url_l; //change url_l to url_o for origonal size or url_m for a medium size
+                photo_element.classList.add('materialboxed', 'pictures', 'z-depth-4');
                 myPhotos.appendChild(photo_element);
-                photo_element.classList.add('materialboxed'); // Adds zoom on pictures
-                photo_element.classList.add('pictures'); // photo_element.style.cssText = `align-self: center; cursor: pointer; max-width: 100%; max-height: 197.9295px; border-radius: 4px; transition: box-shadow .2s`; also max-height: 470px margin: auto auto centers photos within dev container
-                photo_element.classList.add('z-depth-4'); // Adds shadow on pictures
             }
             materialboxedIntances();
-        })
+        });
 
     //fetching pictures from specific album
     //assigns album photos to paralPics elements (paralPics0 & paralPics1)
     fetch(link_albumPhotos)
         .then(response => response.json())
         .then(data => {
-            console.log(data);
-            let photoArray = data.photoset.photo;
+            const photoArray = data.photoset.photo;
             for (let i = 0; i < photoArray.length; i++) {
                 let paralPic = document.getElementById(`paralPic${i}`);
-                console.log(`paralPic${i}`);
                 let photo = photoArray[i].url_o;
                 paralPic.setAttribute('src', photo);
                 paralPic.style.cssText = `max-width: 100vw; min-width: 25%`;
@@ -62,13 +56,9 @@ document.addEventListener('DOMContentLoaded', function () {
     function materialboxedIntances() {
         let materialBoxedElements = document.querySelectorAll('.materialboxed');
         let materialBoxedInstances = M.Materialbox.init(materialBoxedElements);
-
         let scrollSpyElements = document.querySelectorAll('.scrollspy');
         let scrollSpyOptions = {scrollOffset: 0} 
         let scrollSpyInstances = M.ScrollSpy.init(scrollSpyElements, scrollSpyOptions);
-
-        // let pushPinElements = document.querySelectorAll('.pushpin');
-
     }
 
     // Initialize parallax 
@@ -76,5 +66,5 @@ document.addEventListener('DOMContentLoaded', function () {
         let elem = document.querySelectorAll('.parallax');
         let instances = M.Parallax.init(elem);
     }
+
 });
-//calculate height of camera picture
